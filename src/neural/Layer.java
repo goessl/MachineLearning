96659a8124d41baedbbdff60673c10d8f1e51520
Sebastian Gössl <sebig3000@gmail.com>
2017-06-30 00:21:54 +0200

    Init commit

---
+++ src/neural/Layer.java
@@ -0,0 +1,274 @@
+package neural;
+
+import java.util.Arrays;
+
+/**
+ * Layer of a neural network
+ * 
+ * @author Sebastian Gössl
+ * @version 0.9 28.06.2017
+ */
+public class Layer
+{
+  //Activation functions
+  /** None. Just outputs the sum of all inputs **/
+  public static final int ACTIVATION_NONE = 0;
+  /** Hyperbolic tangent. Output between -1 and 1 **/
+  public static final int ACTIVATION_TANH = 1;
+  /** Sigmoid / Logistic function. Output between 0 and 1 **/
+  public static final int ACTIVATION_SIGMOID = 2;
+  /** Step function. Everything smaller than 0 becomes -1, everything else 1 **/
+  public static final int ACTIVATION_HEAVYSIDE_STEP_FUNCTION_N1_1 = 3;
+  /** Step function. Everything smaller than 0 becomes 0, everything else 1 **/
+  public static final int ACTIVATION_HEAVYSIDE_STEP_FUNCTION_0_1 = 4;
+  /** Rectified Linear Unit (ReLU). Everything below 0 becomes 0 **/
+  public static final int ACTIVATION_RECTIFIED_LINEAR_UNIT = 5;
+  
+  //Some numbers to define the size
+  /** Number of inputs. **/
+  private final int input_n;
+  /** Number of outputs. **/
+  private final int output_n;
+  /** Weights. Every input gets an array of weights. One for every output **/
+  private double[][] weights;
+  /** Activation function. Applied in every neuron on the sum of the weighted inputs **/
+  private final int activationFunction;
+  
+  
+  
+  /**
+   * Constructs a new Layer
+   * @param input_n Number of inputs
+   * @param output_n Number of outputs
+   * @param activationFunction Activation function to apply in every neuron
+   * @throws Exception If number of input or output neurons doesn't fit
+   */
+  public Layer(int input_n, int output_n,
+          int activationFunction) throws Exception
+  {
+    if(input_n <= 0)
+      throw new Exception("Number of inputs must be 1 or more!");
+    if(output_n <= 0)
+      throw new Exception("Number of outputs must be 1 or more!");
+    
+    
+    this.input_n = input_n;
+    this.output_n = output_n;
+    this.activationFunction = activationFunction;
+    
+    //Every input has an array of weights, one for every output
+    //(Prepare for some awesome ASCII art!)
+    //
+    //  I1----W11----Z1
+    //    \         /
+    //     --W12-\ /
+    //            \
+    //     --W21-/ \
+    //    /         \
+    //  I2----W22----Z2
+    //
+    // Z1 = W11*I1 + W21*I2
+    // Z2 = W12*I1 + W22*I2
+    //
+    //  In conclusion: weights[In][Zn]
+    //  In: Input n, Zn: Neuron n
+    //
+    weights = new double[input_n][output_n];
+  }
+  
+  
+  /**
+   * Calulates the output based on the given input
+   * @param input Input to feed into the layer
+   * @return The calculated utput of the layer
+   * @throws Exception If the size of the input doesn't fit
+   */
+  public double[] calculate(double[] input) throws Exception
+  {
+    if(input.length != input_n)
+      throw new Exception("Number of inputs doesn't fit!");
+    
+    
+    double[] output = new double[output_n];
+    
+    //Sum all weighted inputs for every output neuron
+    //For every output ...
+    for(int j=0; j<output.length; j++)
+      //... sum all the weighted inputs
+      for(int i=0; i<input.length; i++)
+        output[j] += weights[i][j] * input[i];
+    
+    
+    //Apply the activation function on every output neuron
+    for(int i=0; i<output.length; i++)
+      switch(activationFunction)
+      {
+        //None. Just outputs the sum of all inputs
+        case ACTIVATION_NONE:
+          break;
+        
+        //Hyperbolic tangent. Output between -1 and 1
+        case ACTIVATION_TANH:
+          output[i] = Math.tanh(output[i]);
+          break;
+        
+        //Sigmoid / Logistic function. Output between 0 and 1
+        case ACTIVATION_SIGMOID:
+          output[i] = 1/(1+ Math.exp(-output[i]));
+          break;
+        
+        //Step function. Everything smaller than 0 becomes -1, everything else 1
+        case ACTIVATION_HEAVYSIDE_STEP_FUNCTION_N1_1:
+          if(output[i] >= 0)
+            output[i] = 1;
+          else
+            output[i] = 0;
+          break;
+        
+        //Step function. Everything smaller than 0 becomes 0, everything else 1
+        case ACTIVATION_HEAVYSIDE_STEP_FUNCTION_0_1:
+          if(output[i] >= 0)
+            output[i] = 1;
+          else
+            output[i] = 0;
+          break;
+        
+        //Rectified Linear Unit (ReLU). Everything below 0 becomes 0
+        case ACTIVATION_RECTIFIED_LINEAR_UNIT:
+          if(output[i] < 0)
+            output[i] = 0;
+          break;
+        
+        
+        default:
+          ;
+      }
+    
+    return output;
+  }
+
+  /**
+   * Initializes all weights with random values
+   * @param minimum Minimal value a weight can have
+   * @param maximum Maximal value a weight can have
+   */
+  public void seedWeights(double minimum, double maximum)
+  {
+    for(double[] weightArray : weights)
+      for(int i=0; i<weightArray.length; i++)
+        weightArray[i] = (maximum-minimum)*Math.random() + minimum;
+  }
+  
+  
+  /**
+   * Sets all weights
+   * @param weights Array of weight arrays.
+   * Every input gets an array of weights, one for every output
+   * @throws Exception If the array size doesn't fit
+   */
+  public void setWeights(double[][] weights) throws Exception
+  {
+    //As many arrays as inputs?
+    if(weights.length != this.weights.length)
+      throw new Exception("Wrong size!");
+    //As many weights as outputs, for every input?
+    for(int i=0; i<this.weights.length; i++)
+      if(weights[i].length != this.weights[i].length)
+        throw new Exception("Wrong size!");
+
+
+    this.weights = weights;
+  }
+  
+  
+  /**
+   * @return Weights
+   */
+  public double[][] getWeights()
+  {
+    return weights;
+  }
+  
+  /**
+   * @return Number of inputs
+   */
+  public int getInput_n()
+  {
+    return input_n;
+  }
+  
+  /**
+   * @return Number of outputs
+   */
+  public int getOutput_n()
+  {
+    return output_n;
+  }
+  
+  /**
+   * @return Activation function
+   */
+  public int getActivationFunction()
+  {
+    return activationFunction;
+  }
+  
+  
+  
+  //Output a more or less readable representation
+  @Override
+  public String toString()
+  {
+    //Construct a beautiful string!
+    String result = "";
+    
+    //All the weights
+    //Every column shows all the weights for one input
+    //Every row show all the weights for one output
+    for(int j=0; j<output_n; j++)
+    {
+      for(int i=0; i<input_n; i++)
+        result += String.format("| %.5f ", weights[i][j]);
+      result += "|\n";
+    }
+    
+    //Draw an o for every output with an "/ \" above, if there are more outputs
+    //than inputs (layer gets bigger),
+    //or an "\ /" if it gets smaller or "|" if the size stays the same
+    for(int i=0; i<output_n; i++)
+    {
+      if(output_n < input_n)
+        result += "    \\ /   ";
+      else
+        if(output_n > input_n)
+          result += "    / \\   ";
+        else
+          result += "     |    ";
+    }
+    result += "\n";
+    
+    for(int i=0; i<output_n; i++)
+      result += "     o    ";
+    result += "\n";
+    
+    
+    //Return out beautiful string!
+    return result;
+  }
+  
+  
+  
+  //Test everything!
+  public static void main(String[] args) throws Exception
+  {
+    //4 inputs, 4 neurons, tanh as activation function
+    Layer layer = new Layer(4, 4, Layer.ACTIVATION_TANH);
+    
+    layer.seedWeights(-1, 1);
+    System.out.println(layer);
+    
+    double[] input = new double[]{1, 0, 0.5, -1};
+    System.out.println(Arrays.toString(input));
+    System.out.println(Arrays.toString(layer.calculate(input)));
+  }
+}
---
+++ src/neural/Network.java
@@ -0,0 +1,349 @@
+package neural;
+
+import java.util.Arrays;
+
+/**
+ * Neural network
+ * 
+ * @author Sebastian Gössl
+ * @version 0.9 28.06.2017
+ */
+public class Network
+{
+  /** Layers. All the Layers in the Network **/
+  private final Layer[] layers;
+  
+  
+  
+  /**
+   * Constructor a new neural network
+   * @param input_n The number of input neurons
+   * @param layerSizes An array with the number of neurons for every layer.
+   * The last number of neurons are the output neurons
+   * @param activationFunctions An array with the activation functions
+   * for every layer
+   * @throws Exception 
+   */
+  public Network(int input_n, int[] layerSizes,
+          int[] activationFunctions) throws Exception
+  {
+    if(layerSizes.length <= 0)
+      throw new Exception("Number of hidden layers must be more than 0!");
+    if(activationFunctions.length != layerSizes.length)
+      throw new Exception
+        ("There must be as many activation functions as hidden layers!");
+    
+    
+    //Declare the layers ...
+    layers = new Layer[layerSizes.length];
+    
+    //... and initialize them so
+    //that every layer has as many neurons as it should have,
+    //and as many inputs as the previous layer has neurons
+    //First layer
+    layers[0] = new Layer(input_n, layerSizes[0], activationFunctions[0]);
+    //Hidden layers + output layer
+    for(int i=1; i<layerSizes.length; i++)
+      layers[i] = new Layer(layerSizes[i-1], layerSizes[i],
+              activationFunctions[i]);
+  }
+  
+  
+  /**
+   * Calulates the output based on the given input. Forward propagation
+   * @param input The input to feed into the network
+   * @return The calculated output of the network
+   * @throws Exception If an error in a layer occurs
+   */
+  public double[] calculate(double[] input) throws Exception
+  {
+    //Take the input ...
+    double[] temp  = input;
+    
+    //... and feed it into a layer and feed  the output into the next layer
+    for (Layer layer : layers)
+      temp = layer.calculate(temp);
+    
+    return temp;
+  }
+  
+  /**
+   * Trains the network with the given input and output
+   * @param loops How often it tests all the data sets
+   * @param step How much the weights are adjusted after every test
+   * @param weightMinimum The minimum value a weight can have
+   * @param weightMaximum The maximum value a weight can have
+   * @param input The given training inputs
+   * @param output The given training output
+   * @return Documentation of the costs. Only for graphical/debbug purpose
+   * @throws Exception If the input & output sizes don't match
+   */
+  public double[] train(int loops, double step,
+          double weightMinimum, double weightMaximum,
+          double[][] input, double[][] output) throws Exception
+  {
+    if(input.length != output.length)
+      throw new Exception
+        ("Number of inputs must equal the number of given outputs!");
+    
+    double[] costs = new double[loops];
+    
+    //Adjust the network many times
+    for(int k=0; k<loops; k++)
+    {
+      //Adjust every layer
+      for(Layer layer : layers)
+      {
+        //Save the weights of the current layer
+        double[][] weights = layer.getWeights();
+        
+        //Adjust every weight ...
+        for(int j=0; j<weights.length; j++)
+          for(int i=0; i<weights[j].length; i++)
+          {
+            //Calculate the costs
+            //Costs with the current weights
+            double cost_without_change = cost(input, output);
+            
+            //Costs after increasing the weight
+            weights[j][i] += step;
+            layer.setWeights(weights);
+            double cost_with_increased_weight = cost(input, output);
+            weights[j][i] -= step;
+            
+            //Costs after decreasing the weight
+            weights[j][i] -= step;
+            layer.setWeights(weights);
+            double cost_with_decreased_weight = cost(input, output);
+            weights[j][i] += step;
+            
+            
+            //Compare the costs
+            //Costs with decreased weight are the smallest
+            if(cost_with_decreased_weight < cost_without_change &&
+                    cost_with_decreased_weight < cost_with_increased_weight)
+            {
+              weights[j][i] -= step;
+              if(weights[j][i] < weightMinimum)
+                weights[j][i] = weightMinimum;
+            }
+            else
+            {
+              //Costs with increased weight are the smallest
+              if(cost_with_increased_weight < cost_without_change)
+              {
+                weights[j][i] += step;
+                if(weights[j][i] > weightMaximum)
+                  weights[j][i] = weightMaximum;
+              }
+              //Otherwise no change is optimal
+            }
+            
+            
+            //Update the layer
+            layer.setWeights(weights);
+
+          }//Weights
+      }//Layers
+      
+      costs[k] = cost(input, output);
+      //For debugging output progress
+      System.out.println(100.0*k/loops + "%: " + costs[k]);
+      
+    }//Loops
+    
+    
+    return costs;
+  }
+  
+  /**
+   * Caluclates the costs with the given test inputs and outputs.
+   * Costs are the mean squared error.
+   * @param input The given test inputs
+   * @param output The given test output
+   * @return The costs of the given test set
+   * @throws Exception If the input & output sizes don't match
+   */
+  public double cost(double[][] input, double[][] output) throws Exception
+  {
+    if(input.length != output.length)
+      throw new Exception("Number of inputs must equal the number of given outputs!");
+    
+    
+    double cost = 0;
+    
+    //Sum the costs for every data set
+    for(int j=0; j<input.length; j++)
+    {
+      //Calculate the actual result
+      double[] result = calculate(input[j]);
+      double error = 0;
+      //And compare every actual output with the wanted output
+      for(int i=0; i<result.length; i++)
+        error += (output[j][i] - result[i]) * (output[j][i] - result[i]);
+      cost += error*error;
+    }
+    
+    cost *= 0.5;
+    
+    return cost;
+  }
+  
+  /**
+   * Initalizes all weights of all layers with numbers between -1 and 1
+   * @param minimum Minimal value a weight can have
+   * @param maximum Maximal value a weight can have
+   */
+  public void seedWeights(double minimum, double maximum)
+  {
+    //Seed the weights of every layer
+    for(Layer layer : layers)
+      layer.seedWeights(minimum, maximum);
+  }
+  
+  
+  /**
+   * Sets the weights of all layers
+   * @param weights The weights. Every layer needs a 2-dimensional weight array
+   * @throws Exception If the number of weight sets doesn't fit
+   */
+  public void setWeights(double[][][] weights) throws Exception
+  {
+    if(weights.length != layers.length)
+      throw new Exception("There must be as many weight arrays as layers!");
+    
+    for(int i=0; i<layers.length; i++)
+      layers[i].setWeights(weights[i]);
+  }
+  
+  /**
+   * Sets the weights of a given layer
+   * @param layer Index of the layer of which the weights should be set
+   * @param weights The weights for the layer
+   * @throws Exception If an error occurs in the layer
+   */
+  public void setWeights(int layer, double[][] weights) throws Exception
+  {
+    layers[layer].setWeights(weights);
+  }
+  
+  
+  /**
+   * @return All layers
+   */
+  public Layer[] getLayers()
+  {
+    return layers;
+  }
+  
+  /**
+   * @param index Index of the layer that should be returned
+   * @return The wanted layer
+   */
+  public Layer getLayer(int index)
+  {
+    return layers[index];
+  }
+  
+  
+  
+  //Output a more or less readable representation
+  @Override
+  public String toString()
+  {
+    String result = "";
+    
+    //Just every layer one after another
+    for(int i=0; i<layers.length; i++)
+      result += layers[i].toString();
+    
+    return result;
+  }
+  
+  
+  
+  //Test everything!
+  public static void main(String[] args) throws Exception
+  {
+    /**
+     * This example is based on Brandon Rohrer's "How Deep Neural Networks Work"
+     * https://www.youtube.com/watch?v=ILsA4nyG7I0
+     * or
+     * https://brohrer.github.io/how_neural_networks_work.html
+     * Check it out. It's a great explanation how neural networks work.
+     * 
+     * Input:
+     * The network takes a 4 pixel image
+     *  0 1
+     *  3 4  = {0, 1, 2, 3}
+     * With values between -1 and 1
+     * 
+     * Output:
+     * It outputs a 1 in the corresponding output neuron if the image is solid,
+     * or if there is a vertical, diagonal or horizontal line
+     * {solid, vertical, diagonal, horizontal}
+     */
+    
+    //4 inputs, 4 layers with 4, 4, 8, 4 neurons
+    Network net = new Network(4, new int[]{4, 4, 8, 4},
+            new int[]{Layer.ACTIVATION_TANH, Layer.ACTIVATION_TANH,
+              Layer.ACTIVATION_RECTIFIED_LINEAR_UNIT, Layer.ACTIVATION_NONE});
+    
+    //Randomize all weights
+    net.seedWeights(-1, 1);
+    System.out.println(net);
+    
+    
+    //Train or set the weights manually
+    double[][] train_input = new double[][]{{ 1,  1,  1,  1},
+                                            {-1, -1, -1, -1},
+                                            {-1,  1,  1, -1},
+                                            { 1, -1, -1,  1},
+                                            { 1, -1,  1, -1},
+                                            {-1,  1, -1,  1},
+                                            { 1,  1, -1, -1},
+                                            {-1, -1,  1,  1}};
+    double[][] train_output = new double[][]{{1, 0, 0, 0},
+                                            {1, 0, 0, 0},
+                                            {0, 1, 0, 0},
+                                            {0, 1, 0, 0},
+                                            {0, 0, 1, 0},
+                                            {0, 0, 1, 0},
+                                            {0, 0, 0, 1},
+                                            {0, 0, 0, 1}};
+    
+//    net.setWeights(0, new double[][]{{ 1,  0,  1,  0},
+//                                      { 0,  1,  0,  1},
+//                                      { 0,  1,  0, -1},
+//                                      { 1,  0, -1,  0}});
+//    net.setWeights(1, new double[][]{{ 1, -1,  0,  0},
+//                                      { 1,  1,  0,  0},
+//                                      { 0,  0,  1,  1},
+//                                      { 0,  0, -1, 1}});
+//    net.setWeights(2, new double[][]{{ 1, -1,  0,  0,  0,  0,  0,  0},
+//                                      { 0,  0,  1, -1,  0,  0,  0,  0},
+//                                      { 0,  0,  0,  0,  1, -1,  0,  0},
+//                                      { 0,  0,  0,  0,  0,  0,  1, -1}});
+//    net.setWeights(3, new double[][]{{ 1,  0,  0,  0},
+//                                      { 1,  0,  0,  0},
+//                                      { 0,  1,  0,  0},
+//                                      { 0,  1,  0,  0},
+//                                      { 0,  0,  1,  0},
+//                                      { 0,  0,  1,  0},
+//                                      { 0,  0,  0,  1},
+//                                      { 0,  0,  0,  1}});
+    
+    
+    //Train the network
+    net.train(1000, 0.002, -1, 1, train_input, train_output);
+    net.train(1000, 0.00002, -1, 1, train_input, train_output);
+    
+    System.out.println("\n" + net);
+    System.out.println("Costs: " + net.cost(train_input, train_output));
+    
+    //Show the input and the resulting output
+    for(double[] input : train_input)
+      System.out.println(Arrays.toString(input) + ": " +
+              Arrays.toString(net.calculate(input)));
+  }
+}
